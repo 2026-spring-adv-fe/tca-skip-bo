998df6c1d729dba658c3d2505d8f9ab06c7ae61d
Noah Pixler <npixler@madisoncollege.edu>
2026-02-18 13:30:55 -0600

    Cleared everything out

--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,6 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App 
-      initialCountValue={10}
-      //foo="Noah was here"
-    />
+    <App />
   </StrictMode>,
 )
